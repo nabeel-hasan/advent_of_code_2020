c1690da4db825729fa590f59eff06b825fe7047d
nabeel-hasan <nabeel.io@outlook.com>
2020-12-28 11:29:00 +0530

    added ceil to tree_counting function

--- problem3.py
+++ problem3.py
@@ -5,37 +5,11 @@ import pandas as pd
 df = pd.read_csv('inputs/problem3.csv', header = None)
 df = df.iloc[:, 0].str.split('',expand = True)
 df = df.iloc[:,1:df.shape[1] - 1]
-df
-
 df = df.replace('.',0)
 df = df.replace('#',1)
 df = df.to_numpy()
-df
+dfshape
 
-x = np.copy(df)
-y = int(math.ceil(df.shape[0]*5)/df.shape[1])
-for i in range(y):
-    df = np.hstack((df,x))
-
-df.shape
-
-
-row = 0
-col = 0
-tree = 0
-for i in range(df.shape[0]):
-    if(df[row, col] == 1):
-        tree = tree + 1
-    else:
-        tree = tree + 0
-    row = row + 1
-    col = col + 5
-
-print('The number of tree are :', tree )
-
-
-
-#======================== Part 2 =======================#
 
 def map_stacking(right_steps):
     matrix = np.copy(df)
@@ -46,13 +20,12 @@ def map_stacking(right_steps):
     return matrix
 
 
-
 def tree_counting(right_steps, down_steps):
     row = 0
     col = 0
     tree = 0
     tree_map = map_stacking(right_steps)
-    for i in range(int(tree_map.shape[0]/down_steps)):
+    for i in range(math.ceil(tree_map.shape[0]/down_steps)):
         if(tree_map[row, col] == 1):
          tree = tree + 1
         else:
@@ -62,6 +35,13 @@ def tree_counting(right_steps, down_steps):
     return tree
 
 
+
+#=========== Part 1 ============#
+
+print('Number of trees along the slope :',tree_counting(3,1))
+
+
+#=========== Part2 ============#
 right = np.array([1,3,5,7,1])
 down  = np.array([1,1,1,1,2])
 prod = 1
@@ -71,12 +51,6 @@ for i in range(right.size):
      prod = prod * tree
 
 print('The product is :', prod)
-
-
-
-
-
-
 
 
 
